4a762bfea1cc83f0a1c4c40b7423697eaa8f6160
äbubakar20-02 <abubakarx72@gmail.com>
2023-02-06 00:23:49 +0000

    converted to class

--- main.py
+++ main.py
@@ -4,99 +4,93 @@ from xlsxwriter import Workbook
 import os
 
 
-def generate_initials(first_name, last_name):
-    initials = first_name[0].lower() + last_name[0].lower()
-    return initials
+class User:
+    def __init__(self):
+        super().__init__()
+        self.conn = sqlite3.connect('test_database')
+        self.c = self.conn.cursor()
 
+    def generate_initials(self, first_name, last_name):
+        initials = first_name[0].lower() + last_name[0].lower()
+        return initials
 
-def generate_unique_initials(conn, first_name, last_name):
-    initials = generate_initials(first_name, last_name)
-    i = 1
-    while True:
-        cursor = conn.cursor()
-        cursor.execute("SELECT COUNT(*) FROM User WHERE User_Id = ?", (initials,))
-        count = cursor.fetchone()[0]
-        if count == 0:
-            return initials
-        initials = generate_initials(first_name, last_name) + str(i)
-        i += 1
+    def generate_unique_initials(self, first_name, last_name):
+        initials = self.generate_initials(first_name, last_name)
+        i = 1
+        while True:
+            cursor = self.c
+            cursor.execute("SELECT COUNT(*) FROM User WHERE User_Id = ?", (initials,))
+            count = cursor.fetchone()[0]
+            if count == 0:
+                return initials
+            initials = self.generate_initials(first_name, last_name) + str(i)
+            i += 1
 
+    def deleteTable(self):
+        self.c.execute("DROP TABLE User")
 
-conn = sqlite3.connect('test_database')
-c = conn.cursor()
-
-
-def deleteTable(c):
-    c.execute("DROP TABLE User")
-
-
-deleteTable(c)
-
-
-def createTable(c):
-    c.execute('''
-          CREATE TABLE IF NOT EXISTS User
-          ([User_ID] VARCHAR PRIMARY KEY, [FirstName] TEXT , [LastName] TEXT, [Money] REAL, [Gold] REAL)
-          ''')
-    conn.commit()
-
-
-createTable(c)
-
-
-def deleteRecord(c, User_ID):
-    c.execute('''
-          DELETE FROM User WHERE User_Id = ?
-          ''', (User_ID,))
-    conn.commit()
-
-
-def insertIntoTable(c, FName, LName, Money, Gold):
-    c.execute('''
-          INSERT INTO User (User_ID, FirstName,LastName,Money,Gold)
-
-                VALUES
-                (?,?,?,?,?)
-          ''', (generate_unique_initials(conn, FName, LName), FName, LName, Money, Gold))
-    conn.commit()
-
-
-def updateRecord(c, User_ID, Money, Gold):
-    c.execute('''
-          UPDATE User SET Money = ? , Gold = ? WHERE User_ID = ?
-          ''', (Money, Gold, User_ID))
-    conn.commit()
-
-
-insertIntoTable(c, "Muhammad", "Abubakar", 123.1, 11)
-insertIntoTable(c, "Muhammad", "Abubakar", 123.1, 11)
-insertIntoTable(c, "Muhammad", "Abubakar", 123.1, 11)
-deleteRecord(c, "ma1")
-insertIntoTable(c, "Muhammad", "Abubakar", 123.1, 12)
-updateRecord(c, "ma2", 10.1, 10)
-insertIntoTable(c, "Hamza", "Rizwan", 10, 1)
-updateRecord(c, "hr", 12.6, 1234)
-insertIntoTable(c, "Hamza", "Rizwan", 10, 1)
-
-
-def showTable(c):
-    c.execute('''
-              SELECT * FROM User
+    def createTable(self):
+        self.c.execute('''
+              CREATE TABLE IF NOT EXISTS User
+              ([User_ID] VARCHAR PRIMARY KEY, [FirstName] TEXT , [LastName] TEXT, [Money] REAL, [Gold] REAL)
               ''')
-    df = pd.DataFrame(c.fetchall(), columns=['User_ID', 'FirstName', 'LastName', 'Money', 'Gold'])
-    print(df)
-    convertToExcel(c)
+        self.conn.commit()
+
+    def deleteRecord(self, User_ID):
+        self.c.execute('''
+              DELETE FROM User WHERE User_Id = ?
+              ''', (User_ID,))
+        self.conn.commit()
+
+    def insertIntoTable(self, FName, LName, Money, Gold):
+        self.c.execute('''
+              INSERT INTO User (User_ID, FirstName,LastName,Money,Gold)
+
+                    VALUES
+                    (?,?,?,?,?)
+              ''', (self.generate_unique_initials(FName, LName), FName, LName, Money, Gold))
+        self.conn.commit()
+
+    def updateRecord(self, User_ID, Money, Gold):
+        self.c.execute('''
+              UPDATE User SET Money = ? , Gold = ? WHERE User_ID = ?
+              ''', (Money, Gold, User_ID))
+        self.conn.commit()
+
+    def showTable(self):
+        self.c.execute('''
+                  SELECT * FROM User
+                  ''')
+        df = pd.DataFrame(self.c.fetchall(), columns=['User_ID', 'FirstName', 'LastName', 'Money', 'Gold'])
+        print(df)
+        self.convertToExcel()
+
+    def convertToExcel(self):
+        workbook = Workbook('output.xlsx')
+        worksheet = workbook.add_worksheet()
+        self.c.execute("select * from User")
+        mysel = self.c.execute("select * from User")
+        for i, row in enumerate(mysel):
+            for j, value in enumerate(row):
+                worksheet.write(i + 1, j, value)
+        workbook.close()
+        os.system('output.xlsx')
 
 
-def convertToExcel(c):
-    workbook = Workbook('output.xlsx')
-    worksheet = workbook.add_worksheet()
-    c.execute("select * from User")
-    mysel = c.execute("select * from User")
-    for i, row in enumerate(mysel):
-        for j, value in enumerate(row):
-            worksheet.write(i+1, j, value)
-    workbook.close()
-    os.system('output.xlsx')
+User = User()
 
-showTable(c)
+User.deleteTable()
+
+User.createTable()
+
+User.insertIntoTable("Muhammad", "Abubakar", 123.1, 11)
+User.insertIntoTable("Muhammad", "Abubakar", 123.1, 11)
+User.insertIntoTable("Muhammad", "Abubakar", 123.1, 11)
+User.deleteRecord("ma1")
+User.insertIntoTable("Muhammad", "Abubakar", 123.1, 12)
+User.updateRecord("ma2", 10.1, 10)
+User.insertIntoTable("Hamza", "Rizwan", 10, 1)
+User.updateRecord("hr", 12.6, 1234)
+User.insertIntoTable("Hamza", "Rizwan", 10, 1)
+
+User.showTable()
