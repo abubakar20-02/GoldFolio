5f80f48c06859179da7f48f2df9f844bd9397dd3
äbubakar20-02 <abubakarx72@gmail.com>
2023-02-23 03:50:49 +0000

    created a snapshot function for the database.

--- SetUpFile.py
+++ SetUpFile.py
@@ -6,3 +6,8 @@ DBArchiveName = 'Archive.db'
 
 DBLog = 'Log.db'
 
+Snap_DB = 'SnapDatabase.db'
+
+Snap_Archive = 'SnapArchive.db'
+
+Snap_DBLog = 'SnapDB.db'
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
 import time
 
+import SetUpFile
 from User import User
 from Investment import Investment
 from Statement import Statement
@@ -23,6 +25,44 @@ def method_name():
     ClearArchive()
 
 
+def saveSnapshot():
+    conn = sqlite3.connect(SetUpFile.DBName)
+
+    # Create a file object to store the snapshot
+    snapshot_file = sqlite3.connect(SetUpFile.Snap_DB)
+
+    # Take a snapshot of the database
+    conn.backup(snapshot_file)
+
+    # Close the file object and the database connection
+    snapshot_file.close()
+    conn.close()
+
+    conn = sqlite3.connect(SetUpFile.DBArchiveName)
+
+    # Create a file object to store the snapshot
+    snapshot_file = sqlite3.connect(SetUpFile.Snap_Archive)
+
+    # Take a snapshot of the database
+    conn.backup(snapshot_file)
+
+    # Close the file object and the database connection
+    snapshot_file.close()
+    conn.close()
+
+    conn = sqlite3.connect(SetUpFile.DBLog)
+
+    # Create a file object to store the snapshot
+    snapshot_file = sqlite3.connect(SetUpFile.Snap_DBLog)
+
+    # Take a snapshot of the database
+    conn.backup(snapshot_file)
+
+    # Close the file object and the database connection
+    snapshot_file.close()
+    conn.close()
+
+
 method_name()
 
 # start temp db to store commands that are run. so we can backtrack if needed.
@@ -41,25 +81,26 @@ User.createTable()
 User.ImportFromExcel()
 
 timer = 1
-User.insertIntoTable("Muhammad", "Abubakar", 100)
+# User.insertIntoTable("Muhammad", "Abubakar", 100)
 Investment.ImportFromExcel()
 Statement.ImportFromExcel()
-# time.sleep(timer)
-# User.insertIntoTable("Hamza", "Rizwan",100)
-# time.sleep(timer)
-# User.updateRecord("ma", 20)
-# time.sleep(timer)
-# User.updateRecord("ma", 50)
-# time.sleep(timer)
-# Investment.setProfile("ma")
-# #
-# Investment.insertIntoTable(1, 123, 101)
-# time.sleep(timer)
-# Investment.setProfile("hr")
-# Investment.insertIntoTable(2, 123, 98)
-# time.sleep(timer)
-# Investment.insertIntoTable(3, 3, 50)
-# Investment.sellAll(Rate=50)
+
+time.sleep(timer)
+User.insertIntoTable("Hamza", "Rizwan",100)
+time.sleep(timer)
+User.updateRecord("ma", 20)
+time.sleep(timer)
+User.updateRecord("ma", 50)
+time.sleep(timer)
+Investment.setProfile("ma")
+#
+Investment.insertIntoTable(1, 123, 101)
+time.sleep(timer)
+Investment.setProfile("hr")
+Investment.insertIntoTable(2, 123, 98)
+time.sleep(timer)
+Investment.insertIntoTable(3, 3, 50)
+Investment.sellAll(Rate=50)
 
 
 # a = 10
@@ -69,48 +110,50 @@ Statement.ImportFromExcel()
 #     Investment.updateProfitLoss(a)
 
 
-# Investment.setProfile("hr")
-# Investment.insertIntoTable(123,123,123)
-# Investment.setProfile("ma")
-# Investment.sellAll(Rate=50)
-# Investment.setProfile("hr")
-# time.sleep(timer)
-# Investment.sellAll(Rate=20)
-# time.sleep(timer)
-# # User.deleteRecord("ma")
-# time.sleep(timer)
-# User.insertIntoTable("Ali", "Baba", 1)
-# time.sleep(timer)
-#
-# Investment.setProfile("ab")
-# time.sleep(timer)
-#
-# Investment.insertIntoTable(1, 1, 1, Date="2020-01-21")
-# time.sleep(timer)
-# Investment.insertIntoTable(12, 123, 23)
-# time.sleep(timer)
-# Investment.insertIntoTable(123, 3, 123)
-# time.sleep(timer)
-#
-# Investment.updateProfitLoss(1000)
+Investment.setProfile("hr")
+Investment.insertIntoTable(123,123,123)
+Investment.setProfile("ma")
+Investment.sellAll(Rate=50)
+Investment.setProfile("hr")
+time.sleep(timer)
+Investment.sellAll(Rate=20)
+time.sleep(timer)
+# User.deleteRecord("ma")
+time.sleep(timer)
+User.insertIntoTable("Ali", "Baba", 1)
+time.sleep(timer)
+
+Investment.setProfile("ab")
+time.sleep(timer)
+
+Investment.insertIntoTable(1, 1, 1, Date="2020-01-21")
+time.sleep(timer)
+Investment.insertIntoTable(12, 123, 23)
+time.sleep(timer)
+Investment.insertIntoTable(123, 3, 123)
+time.sleep(timer)
+
+Investment.updateProfitLoss(1000)
+Investment.sellAll()
+
+Investment.updateProfitLoss(1.5)
 # Investment.sellAll()
-#
-# Investment.updateProfitLoss(1.5)
-# # Investment.sellAll()
-# time.sleep(timer)
-#
-# # Investment.sellAll()
-# time.sleep(timer)
-# User.insertIntoTable("m", "s", 90)
-# time.sleep(timer)
-# User.deleteRecord("ms")
-# time.sleep(timer)
-#
-# # method_name()
-# User.insertIntoTable("sa", "as", 100)
-# Investment.setProfile("sa")
-# Investment.insertIntoTable(1, 1, 23)
+time.sleep(timer)
+
 # Investment.sellAll()
+time.sleep(timer)
+User.insertIntoTable("m", "s", 90)
+time.sleep(timer)
+User.deleteRecord("ms")
+time.sleep(timer)
+
+# method_name()
+User.insertIntoTable("sa", "as", 100)
+Investment.setProfile("sa")
+Investment.insertIntoTable(1, 1, 23)
+Investment.sellAll()
+
+saveSnapshot()
 
 
 def previousStage(num):
