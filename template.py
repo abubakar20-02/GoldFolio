c6551b2b5d4049f1e073f85635c8b038b7c0a527
äbubakar20-02 <abubakarx72@gmail.com>
2023-02-24 21:28:06 +0000

    locked scrolling in graph but string still can overlap.

--- template.py
+++ template.py
@@ -1,9 +1,77 @@
 # importing Qt widgets
+from datetime import datetime
+
 from PyQt5.QtWidgets import *
 import sys
 
 # importing pyqtgraph as pg
 import pyqtgraph as pg
+from pyqtgraph import debug as debug
+
+
+class TimeAxisItem(pg.AxisItem):
+    hide_ticks = 0
+
+    def __init__(self, *args, **kwargs):
+        super(TimeAxisItem, self).__init__(*args, **kwargs)
+        # Paint tick every 1 second
+        self.setTickSpacing(levels=[(1, 0)])
+        # Paint tick every 1 minute
+        # self.setTickSpacing(levels=[(60, 0)])
+        # Set fixed tick height
+        self.fixedHeight = 150
+
+    def tickStrings(self, values, scale, spacing):
+        return [int2dt(value).strftime("%Y-%m-%d %H:%M:%S") for value in values]
+
+    def drawPicture(self, p, axisSpec, tickSpecs, textSpecs):
+
+        profiler = debug.Profiler()
+
+        p.setRenderHint(p.RenderHint.Antialiasing, False)
+        p.setRenderHint(p.RenderHint.TextAntialiasing, True)
+
+        ## draw long line along axis
+        pen, p1, p2 = axisSpec
+        p.setPen(pen)
+        p.drawLine(p1, p2)
+        # p.translate(0.5,0)  ## resolves some damn pixel ambiguity
+
+        ## draw ticks
+        for pen, p1, p2 in tickSpecs:
+            p.setPen(pen)
+            p.drawLine(p1, p2)
+        profiler('draw ticks')
+
+        # Draw all text
+        if self.style['tickFont'] is not None:
+            p.setFont(self.style['tickFont'])
+        p.setPen(self.textPen())
+        bounding = self.boundingRect().toAlignedRect()
+        p.setClipRect(bounding)
+        for rect, flags, text in textSpecs:
+            p.save()  # save the painter state
+            p.translate(rect.center())  # move coordinate system to center of text rect
+            p.rotate(-90)  # rotate text
+            p.translate(-rect.center())  # revert coordinate system
+            p.translate(-65, 0)  # Move rotated tick down by 65 pixels
+            p.drawText(rect, int(flags), text)
+            p.restore()  # restore the painter state
+
+        profiler('draw text')
+
+    def tickValues(self, minVal, maxVal, size):
+        if minVal == 0:
+            return []
+        else:
+            ticks = super().tickValues(minVal, maxVal, size)
+            ticks = [(1.0, ticks[0][1][self.hide_ticks:])]
+            return ticks
+
+
+def int2dt(ts):
+    return datetime.fromtimestamp(ts)
+
 
 class MyPlotWidget(pg.PlotWidget):
     def __init__(self, *args, **kwargs):
@@ -18,10 +86,11 @@ class MyPlotWidget(pg.PlotWidget):
                 Result = "Jan"
             else:
                 Result = x
-            y = round(mousePoint.y(),1)
+            y = round(mousePoint.y(), 1)
             QToolTip.showText(self.mapToGlobal(pos), f"x={Result}, y={y}")
         else:
             QToolTip.hideText()
+
 
 class Window(QMainWindow):
 
@@ -58,16 +127,24 @@ class Window(QMainWindow):
         # creating a plot window
         self.plot = MyPlotWidget()
 
+        xdict = {0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct',
+                 10: 'Nov', 11: 'Dec'}
+        stringaxis = pg.AxisItem(orientation='bottom')
+        stringaxis.setTicks([xdict.items()])
+        self.plot.setAxisItems(axisItems={'bottom': stringaxis})
+
+        labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
         # create list for y-axis
-        y1 = [5, 5, 7, 10, 3, 8, 9, 1, 6, 2]
+        y1 = [5, 5, 7, 10, 3, 8, 9, 1, 6, 2, 1, 1]
 
         # create horizontal list i.e x-axis
-        x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
         # create pyqt5graph bar graph item
         # with width = 0.6
         # with bar colors = green
-        bargraph = pg.BarGraphItem(x=x, height=y1, width=0.6, brush='g')
+        bargraph = pg.BarGraphItem(x=x, height=y1, width=0.9, brush='g')
 
         # add item to plot window
         # adding bargraph item to the plot window
@@ -97,28 +174,33 @@ class Window(QMainWindow):
 
     def update(self):
         self.plot.clear()
-        xdict = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June'}
-        stringaxis = pg.AxisItem(orientation='bottom')
-        stringaxis.setTicks([xdict.items()])
-        self.plot.setAxisItems(axisItems={'bottom': stringaxis})
+        self.plot.setMouseEnabled(x=False, y=False)
 
-        x = [1, 2, 3, 4, 5, 6]
-        y1 = [1, 2, 3, 4, 5, 6]
+
+        x = [0,1, 2, 3, 4, 5, 6,7,8,9,10,11]
+        import random
+
+        # Generate 12 random numbers between 0 and 12
+        y1 = [random.randint(0, 100) for _ in range(12)]
+        # y1 = [10, 2, 3, 4, 5, 6,7,8,9,10,11,12]
 
         # create pyqt5graph bar graph item
         # with width = 0.6
         # with bar colors = green
         bargraph = pg.BarGraphItem(x=x, height=y1, width=0.6, brush='g')
+        # Get the y-axis values
+        y_values = bargraph.y()
+        print(y_values)
 
         # add item to plot window
         # adding bargraph item to the plot window
         self.plot.addItem(bargraph)
 
 
-
 if __name__ == "__main__":
     import sys
-# create pyqt5 app
+
+    # create pyqt5 app
     App = QApplication(sys.argv)
 
     # create the instance of our Window
