360ce9be3ec8d2ca64c7b86ee9c6bf27f3fd1875
äbubakar20-02 <abubakarx72@gmail.com>
2023-03-15 21:27:30 +0000

    removed sp 500 platinum and copper from dataset, added future silver.

--- GetDataSet.py
+++ GetDataSet.py
@@ -1,10 +1,14 @@
 import yfinance as yf
 import pandas as pd
 
+# platinum is correlating negatively
+# sp 500 has high correlation with usd index. so removed it
+
 # adj is real exchange rate
 if __name__ == "__main__":
-    start_date = "2012-01-01"
-    end_date = "2023-03-14"
+    # start_date = "2013-01-01"
+    start_date = "2010-01-01"
+    end_date = "2020-03-14"
 
     gold_data = yf.download("GC=F", start=start_date, end=end_date, interval="1d")
     gold_data = gold_data[['Open', 'Close']]
@@ -13,26 +17,26 @@ if __name__ == "__main__":
     gold_data = gold_data.rename(columns=mapping)
     print(gold_data.columns)
 
-    silver_data = yf.download("SI=F", start=start_date, end=end_date, interval="1d")
+    silver_data = yf.download("SIL=F", start=start_date, end=end_date, interval="1d")
     silver_data = silver_data[['Open']]
     print("--------")
     print(silver_data)
     print("--------")
-    mapping = {silver_data.columns[0]: 'Silver Open'}
+    mapping = {silver_data.columns[0]: 'Silver Future Open'}
     silver_data = silver_data.rename(columns=mapping)
     print(silver_data.columns)
 
-    platinum_data = yf.download("PL=F", start=start_date, end=end_date, interval="1d")
-    platinum_data = platinum_data[['Open']]
-    print(type(platinum_data))
-    mapping = {platinum_data.columns[0]: 'Platinum Open'}
-    platinum_data = platinum_data.rename(columns=mapping)
-    print(platinum_data.columns)
+    # platinum_data = yf.download("PL=F", start=start_date, end=end_date, interval="1d")
+    # platinum_data = platinum_data[['Open']]
+    # print(type(platinum_data))
+    # mapping = {platinum_data.columns[0]: 'Platinum Open'}
+    # platinum_data = platinum_data.rename(columns=mapping)
+    # print(platinum_data.columns)
 
-    copper_data = yf.download("HG=F", start=start_date, end=end_date, interval="1d")
-    copper_data = copper_data[['Open']]
-    mapping = {copper_data.columns[0]: 'Copper Open'}
-    copper_data = copper_data.rename(columns=mapping)
+    # copper_data = yf.download("HG=F", start=start_date, end=end_date, interval="1d")
+    # copper_data = copper_data[['Open']]
+    # mapping = {copper_data.columns[0]: 'Copper Open'}
+    # copper_data = copper_data.rename(columns=mapping)
 
     USBond10_data = yf.download("^TNX", start=start_date, end=end_date, interval="1d")
     USBond10_data = USBond10_data[['Open']]
@@ -40,12 +44,12 @@ if __name__ == "__main__":
     mapping = {USBond10_data.columns[0]: 'US Bond 10 Y'}
     USBond10_data = USBond10_data.rename(columns=mapping)
 
-    # download the S&P 500 index data
-    sp500_data = yf.download("^GSPC", start=start_date, end=end_date, interval="1d")
-    sp500_data = sp500_data[['Open']]
-    mapping = {sp500_data.columns[0]: 'S&P 500 Open'}
-    sp500_data = sp500_data.rename(columns=mapping)
-    print(sp500_data)
+    # # download the S&P 500 index data
+    # sp500_data = yf.download("^GSPC", start=start_date, end=end_date, interval="1d")
+    # sp500_data = sp500_data[['Open']]
+    # mapping = {sp500_data.columns[0]: 'S&P 500 Open'}
+    # sp500_data = sp500_data.rename(columns=mapping)
+    # print(sp500_data)
 
     # download the S&P 500 index data
     Usd_Index_data = yf.download("DX-Y.NYB", start=start_date, end=end_date, interval="1d")
@@ -96,9 +100,12 @@ if __name__ == "__main__":
 
     # concatenate the two DataFrames along the column axis
     combined_data = pd.concat(
-        [sp500_data, Usd_Index_data, Oil_data, USDToEuro_data, USDToINR_data, USDToCNY_data, USDToJPY_data,
-         USBond10_data, copper_data, platinum_data, silver_data, gold_data], axis=1)
+        [Usd_Index_data, Oil_data, USDToEuro_data, USDToINR_data, USDToCNY_data, USDToJPY_data,
+         USBond10_data, silver_data, gold_data], axis=1)
 
     # Drop records that don't have gold close.
-    combined_data = combined_data.dropna(subset=['Gold Close'])
+    combined_data = combined_data.dropna()
+    combined_data = combined_data[(combined_data >= 0).all(1)]
+    combined_data.insert(len(combined_data.columns) - 5, 'GoldToOil',
+                         (combined_data['Gold Open'] / combined_data['Oil open']))
     combined_data.to_excel("gold_data.xlsx")
